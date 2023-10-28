02e18740ef74607026a65b7612cca200129864c8
Lucas Borges <diasb.trabalho@gmail.com>
2023-10-28 18:14:41 -0300

    Concluí o segundo exercício

---
+++ solutions/exercise_2.ex
@@ -0,0 +1,11 @@
+ocamilodev = 71255.53
+ocam_l = 30236.23
+camilotk_ = 5256.12
+
+media = (ocamilodev + ocam_l + camilotk_)/3
+
+if floor(media) == 35582 do
+  "Parabéns, você acertou!"
+else
+  "Errooooou!"
+end
